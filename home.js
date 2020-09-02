4c5c0594721f0f8a6d312b771356c62100dc2aaa
Vishal Bollam <vishalbollam139@gmail.com>
2020-09-01 23:16:42 -0700

    first commit

---
+++ home.js
@@ -0,0 +1,117 @@
+console.log('hello ');
+//alert('yoyo');
+// variables 
+var b = 'smoothie';
+console.log(b);
+
+var somenumber = '45';
+console.log(somenumber);
+// adding java script to the HTMl page *****
+//document.getElementById('sometext').innerHTML = 'Hey there'; 
+ //var age = prompt("what is your age?");
+//document.getElementById('sometext').innerHTML = age;
+
+
+
+//dealing with numbers in java script 
+
+var num1=25;
+num1++; // incrementing the value by 1 
+console.log(num1);
+num1--;//decrementing the value by 1
+console.log(num1);
+ // dividing ,multiply*,% remainder 
+ console.log(num1%6);
+ // increment ,decrement by what ever value u want 
+ num1 +=25;
+ console.log(num1);
+
+ //  creating a function
+  function fun(){
+      console.log('what is your name ?')
+  }
+
+// calling a function 
+fun();
+
+
+// create a function that takes in your name and says hello followed by your name 
+/*function greetings (){
+    var name = prompt('what is your name?')
+    console.log('hello'+' '+ name );// string concatenation 
+}
+greetings(); // calling a  function 
+*/
+
+//adding two numbers in a function 
+
+function add( num1, num2){
+    var result=num1+num2;
+    console.log(result);
+
+}
+add(10,25);
+ 
+
+
+// while loops 
+/*var num=0;
+console.log('Natural numbers are as follows');
+while (num<100){
+    num +=1;
+    
+    console.log( num);
+}*/
+// for loops 
+ /*for (let num=0; num<=100;num++){
+    console.log(num);
+ }*/
+
+// common methods of strings in java script 
+
+/*console.log("play with strings ");
+let fruit= 'bananna' ;
+let moreFruits= 'bananna\napple';
+
+console.log(fruit.length);
+console.log(fruit.toUpperCase());
+console.log(fruit.toLowerCase());
+console.log(fruit.slice(2,6));
+console.log(fruit.split(''));
+console.log(fruit.split(','));
+console.log(fruit.charAt(4));
+console.log(fruit.indexOf('b'));
+console.log(fruit.replace('na','13'));
+console.log(fruit[4]);*/
+
+
+
+// arrays in java script 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
