579e1aa27be577d5df8347741c23b4cf03c1ad82
Vishal Bollam <vishalbollam139@gmail.com>
2020-09-02 12:05:45 -0700

    second commit

--- home.js
+++ home.js
@@ -88,7 +88,68 @@ console.log(fruit[4]);*/
 
 // arrays in java script 
 
- 
+ let  fruits=["bananna",'apple',"orange",'pineapples']
+ fruits=new Array("bananna",'apple',"orange",'pineapples')
+
+   console.log(fruits[2])
+   fruits[2]="pear"
+   console.log(fruits);
+for (i=0; i<fruits.length;i++){
+    console.log(fruits[i]);
+}
+
+// array common methods 
+
+console.log(fruits);
+
+console.log('tostring',fruits.toString());
+console.log(fruits.join('*'));
+console.log(fruits.pop(),fruits) // removes last item from array 
+console.log(fruits.push('guava'),fruits);// appends 
+console.log(fruits[4]);
+fruits[fruits.length]='new fruit';
+console.log(fruits);
+fruits.shift();//removes first elemnt from array 
+console.log(fruits);
+fruits.unshift('kiwi');// adds first element to an array
+console.log(fruits);
+
+
+let vegetables= ['asparagus','brocoli','carrot','peas','cucumber']
+let allgroceries=(fruits.concat(vegetables)); //combines an array
+console.log(allgroceries);
+console.log(allgroceries.slice(1,4));
+console.log(allgroceries.reverse());
+  // sorting a regular array 
+  console.log(allgroceries.sort()); // sorts in alphabetical order 
+  //when sorting an array of numbers we have to pass on a filter **********
+  let someNumbers=[5,10,2,25,3,255,1,2,5,334,321,2];
+   console.log(someNumbers.sort(function(a,b){return a-b}));// this will sort in ascending order 
+   console.log(someNumbers.sort(function(a,b){return b-a}));//sort in descending order 
+ // adding elements in to an array 
+   let emptyArray = new Array();
+   for (let num=0;num<10;num++){
+       emptyArray.push(num);
+
+   }
+   console.log(emptyArray);
+   console.log(emptyArray.reverse());
+
+
+
+   // objects in java scripts 
+   
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ practicequestions.js
@@ -0,0 +1,11 @@
+// create a function that takes the sum of two numbers and Return the  sum of two numbers 
+ 
+
+ function adding(num1,num2){
+     var num = num1+num2
+     var finalresult= num ;
+     console.log(finalresult);
+
+ }
+ adding(10,20)
+ 
